64994ea29df16096cebe96a21275ff1e9a543172
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-17 19:51:57 +0800

    1

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,5 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
     <swiper :options="swiperOption" ref="mySwiperA">
       <!-- 幻灯内容 -->
       <swiper-slide>I'm Slide 1</swiper-slide>
@@ -26,75 +9,36 @@
       <swiper-slide>I'm Slide 5</swiper-slide>
       <swiper-slide>I'm Slide 6</swiper-slide>
       <swiper-slide>I'm Slide 7</swiper-slide>
-      <!-- ... -->
-      <!-- 以下控件元素均为可选（使用具名slot来确定并应用一些操作控件元素） -->
       <div class="swiper-pagination"  slot="pagination"></div>
-      <div class="swiper-button-prev" slot="button-prev"></div>
-      <div class="swiper-button-next" slot="button-next"></div>
-      <div class="swiper-scrollbar"   slot="scrollbar"></div>
     </swiper>
   </div>
 </template>
 
 <script>
-import { swiper, swiperSlide, swiperPlugins } from 'vue-awesome-swiper'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
       swiperOption: {
-        // 所有配置均为可选（同Swiper配置）
-        // NotNextTick is a component's own property, and if notNextTick is set to true, the component will not instantiate the swiper through NextTick, which means you can get the swiper object the first time (if you need to use the get swiper object to do what Things, then this property must be true)
-        // notNextTick是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象（假如你需要使用获取swiper对象来做什么事，那么这个属性一定要是true）
-        notNextTick: true,
-        autoplay: 3000,
-        direction : 'vertical',
-        grabCursor : true,
-        setWrapperSize :true,
-        autoHeight: true,
-        pagination : '.swiper-pagination',
-        paginationClickable :true,
-        prevButton:'.swiper-button-prev',
-        nextButton:'.swiper-button-next',
-        scrollbar:'.swiper-scrollbar',
-        mousewheelControl : true,
-        observeParents:true,
-        // if you need use plugins in the swiper, you can config in here like this
-        // 如果自行设计了插件，那么插件的一些配置相关参数，也应该出现在这个对象中，如下debugger
-        debugger: true,
-        // swiper callbacks
-        // swiper的各种回调函数也可以出现在这个对象中，和swiper官方一样
-        onTransitionStart(swiper){
-          console.log(swiper)
-        },
-        // more Swiper config ...
-        // ...
+        pagination: '.swiper-pagination',
+        direction: 'vertical',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        mousewheelControl: true,
+        autoHeight: true
       }
     }
   },
   components: {
       swiper,
       swiperSlide
-  },
-  // example code (if you need to get the current swiper object, you can find the swiper object like this, the $ref object is a ref attribute corresponding to the dom redefined)
-  // 如果你需要得到当前的swiper对象来做一些事情，你可以像下面这样定义一个方法属性来获取当前的swiper对象，实际上这里的$refs对应的是当前组件内所有关联了ref属性的组件元素对象，同时配置中的notNextTick属性一定要设置为true
-  computed: {
-    swiper() {
-      return this.$refs.mySwiperA.swiper
-    }
-  },
-  mounted() {
-    // you can use current swiper object to do something(swiper methods)
-    // 然后你就可以使用当前上下文内的swiper对象去做你想做的事了
-    console.log('this is current swiper object', this.swiper)
-    this.swiper.slideTo(3, 1000, false)
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
