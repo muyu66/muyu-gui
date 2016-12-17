b1ee00ea815d5d3fe370e99804bb59e99175c237
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-17 20:15:46 +0800

    1

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="hello">
         <swiper :options="swiperOption" style="height: 100%">
-            <swiper-slide>I'm Slide 1</swiper-slide>
-            <swiper-slide>I'm Slide 2</swiper-slide>
-            <swiper-slide>I'm Slide 3</swiper-slide>
+            <swiper-slide><div>有朋自远方来，不亦乐乎</div></swiper-slide>
+            <swiper-slide><div>星际的远征 StatLongWaric</div></swiper-slide>
+            <swiper-slide><div>议会 - 星际分会场</div></swiper-slide>
             <div class="swiper-pagination" slot="pagination"></div>
         </swiper>
     </div>
@@ -39,5 +39,6 @@
         background-color: rgb(223, 84, 32);
         height: 100%;
         width: 100%;
+        margin: auto;
     }
 </style>
