3560284a1b2824587a2ffcc1b2dd5b1f27660d27
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-17 20:01:27 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: red;
   height: 100%;
   width: 100%;
   top: 0px;
