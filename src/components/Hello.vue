f595b668aef087ae72004243758bdbb3c7ddc5a3
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-17 20:06:47 +0800

    1

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,46 +1,47 @@
 <template>
-  <div class="hello">
-    <swiper :options="swiperOption" style="height: 100%">
-      <!-- 幻灯内容 -->
-      <swiper-slide>I'm Slide 1</swiper-slide>
-      <swiper-slide>I'm Slide 2</swiper-slide>
-      <swiper-slide>I'm Slide 3</swiper-slide>
-      <swiper-slide>I'm Slide 4</swiper-slide>
-      <swiper-slide>I'm Slide 5</swiper-slide>
-      <swiper-slide>I'm Slide 6</swiper-slide>
-      <swiper-slide>I'm Slide 7</swiper-slide>
-      <div class="swiper-pagination"  slot="pagination"></div>
-    </swiper>
-  </div>
+    <div class="hello">
+        <swiper :options="swiperOption" style="height: 100%">
+            <!-- 幻灯内容 -->
+            <swiper-slide>I'm Slide 1</swiper-slide>
+            <swiper-slide>I'm Slide 2</swiper-slide>
+            <swiper-slide>I'm Slide 3</swiper-slide>
+            <swiper-slide>I'm Slide 4</swiper-slide>
+            <swiper-slide>I'm Slide 5</swiper-slide>
+            <swiper-slide>I'm Slide 6</swiper-slide>
+            <swiper-slide>I'm Slide 7</swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
 </template>
 
 <script>
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
+  import {swiper, swiperSlide} from 'vue-awesome-swiper'
 
-export default {
-  name: 'hello',
-  data () {
-    return {
-      swiperOption: {
-        pagination: '.swiper-pagination',
-        direction: 'vertical',
-        slidesPerView: 1,
-        paginationClickable: true,
-        spaceBetween: 30,
-        mousewheelControl: true,
-        autoHeight: true
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          direction: 'vertical',
+          slidesPerView: 1,
+          paginationClickable: true,
+          spaceBetween: 30,
+          mousewheelControl: true,
+          autoHeight: true
+        }
       }
-    }
-  },
-  components: {
+    },
+    components: {
       swiper,
       swiperSlide
+    }
   }
-}
 </script>
 
 <style scoped>
-    .hello{
+    .hello {
+        height: 100%;
         width: 100%;
     }
 </style>
